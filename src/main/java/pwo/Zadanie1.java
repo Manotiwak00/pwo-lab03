135d4bfdaf52ae148f895de74aa0a8a6354d8731
Mateusz Nowak <manotiwak123@interia.pl>
2023-10-17 16:38:16 +0200

    Wykonanie Zadania 2 - wersja 1

--- src/main/java/pwo/Zadanie1.java
+++ src/main/java/pwo/Zadanie1.java
@@ -4,6 +4,7 @@ public class Zadanie1 {
 
     public static final int FROM = 0, TO = 100;
     public static final String FILE_NAME = "fibseq100.txt";
+    public static final String FILE_NAME2 = "lucasnum100.txt";
 
     public static void main(String[] args) {
 
@@ -18,5 +19,18 @@ public class Zadanie1 {
         } else {
             System.out.println("Błąd");
         }
-    }
+        
+        // ZADANIE 2
+        System.out.println("Lucas wersja 1");
+
+        boolean ok2 = SequenceTools.writeToFile2(
+                new LucasGenerator(), FROM, TO, FILE_NAME2);
+
+        if (ok2) {
+            System.out.println("Wynik zapisany do pliku: "
+                    + FILE_NAME2);
+        } else {
+            System.out.println("Błąd");
+        }
+   }
 }
